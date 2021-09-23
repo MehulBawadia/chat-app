ffab897a789bf4ec7ead1260a9edaabe35261661
Mehul Bawadia <mehulbawadia@gmail.com>
2021-09-23 17:04:36 +0530

    Fetching rooms and messages for the current room

--- resources/js/Pages/Chat/Container.vue
+++ resources/js/Pages/Chat/Container.vue
@@ -9,7 +9,8 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    Container
+                    <message-container />
+                    <input-message :room="currentRoom" />
                 </div>
             </div>
         </div>
@@ -19,10 +20,52 @@
 <script>
     import { defineComponent } from 'vue'
     import AppLayout from '@/Layouts/AppLayout.vue'
+    import MessageContainer from './MessageContainer.vue'
+    import InputMessage from './InputMessage.vue'
 
     export default defineComponent({
         components: {
-            AppLayout
+            AppLayout,
+            InputMessage,
+            MessageContainer
         },
+
+        data: function () {
+            return {
+                chatRooms: [],
+                currentRoom: [],
+                messages: []
+            }
+        },
+
+        methods: {
+            getRooms() {
+                axios.get('/chat/rooms')
+                    .then(response => {
+                        this.chatRooms = response.data;
+                        this.setRoom(response.data[0]);
+                    }).catch(error => {
+                        console.log(error);
+                    });
+            },
+
+            setRoom(room) {
+                this.currentRoom = room;
+                this.getMessages();
+            },
+
+            getMessages() {
+                axios.get('/chat/room/' + this.currentRoom.id + '/messages')
+                    .then(response => {
+                        this.messages = response.data;
+                    }).catch(error => {
+                        console.log(error);
+                    });
+            }
+        },
+
+        created() {
+            this.getRooms();
+        }
     })
 </script>
