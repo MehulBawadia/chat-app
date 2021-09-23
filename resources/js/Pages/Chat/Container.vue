27998aa0e7a17c6b2b9b5edddad150e49d0d9053
Mehul Bawadia <mehulbawadia@gmail.com>
2021-09-23 17:46:20 +0530

    Room selection

--- resources/js/Pages/Chat/Container.vue
+++ resources/js/Pages/Chat/Container.vue
@@ -2,7 +2,7 @@
     <app-layout title="Dashboard">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Chat
+                <room-selection v-if="currentRoom.id" :rooms="chatRooms" :currentRoom="currentRoom" v-on:roomchanged="setRoom($event)" />
             </h2>
         </template>
 
@@ -20,13 +20,15 @@
 <script>
     import { defineComponent } from 'vue'
     import AppLayout from '@/Layouts/AppLayout.vue'
-    import MessageContainer from './MessageContainer.vue'
+    import RoomSelection from './RoomSelection.vue'
     import InputMessage from './InputMessage.vue'
+    import MessageContainer from './MessageContainer.vue'
 
     export default defineComponent({
         components: {
             AppLayout,
             InputMessage,
+            RoomSelection,
             MessageContainer
         },
 
